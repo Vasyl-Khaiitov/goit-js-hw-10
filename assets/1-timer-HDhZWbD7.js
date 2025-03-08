9aaf82e9381cc9a3bad4013da8ec118af11c5abe
Vasyl-Khaiitov <workjobsproject@gmail.com>
2025-03-08 20:48:18 +0000

    Deploying to gh-pages from @ Vasyl-Khaiitov/goit-js-hw-10@6d7d669b1ba46a6a7dd7600fcc4ed665e84d5aa1 🚀

---
+++ assets/1-timer-HDhZWbD7.js
@@ -0,0 +1,2 @@
+import{f as o}from"./vendor-CCO2o_no.js";const t=document.querySelector("[data-start]"),a={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){e[0]<new Date?(window.alert("Please choose a date in the future"),t.disabled=!0):t.disabled=!1,console.log(e[0])}};o("#datetime-picker",a);
+//# sourceMappingURL=1-timer-HDhZWbD7.js.map
